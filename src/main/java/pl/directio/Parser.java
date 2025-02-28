3790ad70e0a47d819416c3954b7f18818ed97dcb
Kacper Lis <kacper.mikolaj.lis@gmail.com>
2025-02-28 15:13:06 +0100

    solution & tests

---
+++ src/main/java/pl/directio/Parser.java
@@ -0,0 +1,122 @@
+package pl.directio;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.StandardException;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.function.BiFunction;
+
+class Parser {
+
+	@RequiredArgsConstructor
+	private enum Operator {
+		ADD(100, Integer::sum),
+		SUBTRACT(100, (a, b) -> a - b),
+		MULTIPLY(200, (a, b) -> a * b),
+		DIVIDE(
+				200, (a, b) -> {
+			if (b == 0) {
+				throw new ArithmeticException("Division by 0");
+			} else {
+				return a / b;
+			}
+		}
+		);
+
+		private final int precedence;
+		private final BiFunction<Integer, Integer, Integer> operation;
+
+		public int apply(
+				int a,
+				int b
+		) {
+			return operation.apply(a, b);
+		}
+	}
+
+	@StandardException
+	public static class InvalidExpressionException extends RuntimeException {
+
+	}
+
+	public int parse(@NonNull String expression) {
+		if (expression.isBlank()) {
+			throw new IllegalArgumentException("Empty or blank expression: \"" + expression + "\"");
+		}
+
+		final Deque<Integer> numbers = new ArrayDeque<>();
+		final Deque<Operator> operators = new ArrayDeque<>();
+
+		final StringBuilder buffer = new StringBuilder();
+		final char[] chars = expression.toCharArray();
+
+		for (int i = 0; i < chars.length; ++i) {
+			final char current = chars[i];
+			final char next = i + 1 < chars.length ? chars[i + 1] : '\0';
+
+			if (Character.isDigit(current) || (current == '-' && Character.isDigit(next))) {
+				buffer.append(current);
+			} else {
+				if (!buffer.isEmpty()) {
+					final int number = Integer.parseInt(buffer.toString());
+					numbers.push(number);
+					buffer.setLength(0);
+				}
+
+				if (Character.isWhitespace(current)) {
+					continue;
+				} else {
+					final Operator newOperator = switch (current) {
+						case '+' -> Operator.ADD;
+						case '-' -> Operator.SUBTRACT;
+						case '*' -> Operator.MULTIPLY;
+						case '/' -> Operator.DIVIDE;
+						default -> throw new InvalidExpressionException("Unknown operator \"" + current + "\"");
+					};
+					while (!operators.isEmpty() && newOperator.precedence <= operators.peek().precedence) {
+						final Operator operator = operators.pop();
+						if (numbers.size() < 2) {
+							throw new InvalidExpressionException("Missing operands for operation: " + operator.name());
+						}
+
+						final Integer b = numbers.pop();
+						final Integer a = numbers.pop();
+						final int result = operator.apply(
+								a,
+								b
+						);
+
+						numbers.push(result);
+					}
+					operators.push(newOperator);
+				}
+			}
+		}
+
+		if (!buffer.isEmpty()) {
+			final int number = Integer.parseInt(buffer.toString());
+			numbers.push(number);
+			buffer.setLength(0);
+		}
+
+		while (!operators.isEmpty()) {
+			final Operator operator = operators.pop();
+			if (numbers.size() < 2) {
+				throw new InvalidExpressionException("Missing operands for operation: " + operator.name());
+			}
+
+			final Integer b = numbers.pop();
+			final Integer a = numbers.pop();
+			final int result = operator.apply(
+					a,
+					b
+			);
+
+			numbers.push(result);
+		}
+
+		return numbers.pop();
+	}
+}
---
+++ src/test/java/pl/directio/ParserTest.java
@@ -0,0 +1,113 @@
+package pl.directio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+class ParserTest {
+
+	private final Parser parser = new Parser();
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"",
+			" ",
+			"\n",
+			"\t",
+			"     "
+	})
+	void throws_on_empty_or_blank(String expression) {
+		Assertions.assertThrows(
+				IllegalArgumentException.class,
+				() -> parser.parse(expression),
+				"Empty or blank expression: \"" + expression + "\""
+		);
+	}
+
+	@Test
+	void throws_on_null() {
+		Assertions.assertThrows(
+				NullPointerException.class,
+				() -> parser.parse(null),
+				"expression is marked non-null but is null"
+		);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"+",
+			"-",
+			"*",
+			"/",
+			"1+",
+			"+1",
+			"1 + 2 +",
+			"-2 -"
+	})
+	void throw_on_missing_operands_expression(String expression) {
+		Throwable t = Assertions.assertThrows(
+				Parser.InvalidExpressionException.class,
+				() -> parser.parse(expression)
+		);
+
+		Assertions.assertTrue(t.getMessage().startsWith("Missing operands for operation:"));
+	}
+
+	@Test
+	void throws_on_division_by_0() {
+		Assertions.assertThrows(
+				ArithmeticException.class,
+				() -> parser.parse("1 / 0"),
+				"Division by 0"
+		);
+	}
+
+	@Test
+	void evaluates_left_to_right() {
+		Assertions.assertEquals(
+				0,
+				parser.parse("1 / 1 * 0")
+		);
+	}
+
+	@Test
+	void throws_on_unknown_symbol() {
+		Assertions.assertThrows(
+				Parser.InvalidExpressionException.class,
+				() -> parser.parse("1 ^ 1"),
+				"Unknown operator \"^\""
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("getHappyPathTestCases")
+	void happy_path(
+			String expression,
+			int expectedResult
+	) {
+		Assertions.assertEquals(
+				expectedResult,
+				parser.parse(expression)
+		);
+	}
+
+	public static Stream<Arguments> getHappyPathTestCases() {
+		return Stream.of(
+				Arguments.of("1", 1),
+				Arguments.of("-1", -1),
+				Arguments.of("1 + -1", 0),
+				Arguments.of("1 + 1", 2),
+				Arguments.of("1 - 1", 0),
+				Arguments.of("1 * 1", 1),
+				Arguments.of("1 / 1", 1),
+				Arguments.of("2 + 3", 5),
+				Arguments.of("3 * 2 + 1", 7),
+				Arguments.of("3 * -2 + 6", 0)
+		);
+	}
+}
